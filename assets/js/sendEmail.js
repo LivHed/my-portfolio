8811cfa7118ad66f59289142b11efc44de068e98
LivHed <livia.hedin@gmail.com>
2020-05-25 15:17:27 +0200

    Add the function for sending mails through the contact form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,17 @@
-console.log('Helloooo');
+function sendMail(contactForm) {
+    emailjs.send("gmail", "contactportfolio", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "contact_request": contactForm.messagefield.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response.status, response.text);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
+
