ad25442952f7eef0f72cbf32e0135fb2cdb42163
LivHed <livia.hedin@gmail.com>
2020-05-25 16:38:44 +0200

    Add alert messages for emailjs, add css styling to reset btn

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,3 +1,8 @@
+// The function takes the EmailJS serviceID (the id of the service through which the email should be sent),
+// the templateID (which is the template id of the email)
+// and the templateParams (parameters of the template)
+// to be able to send an email through the contact form. 
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "contactportfolio", {
         "from_name": contactForm.name.value,
@@ -6,12 +11,15 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
-            console.log("SUCCESS", response.status, response.text);
+            console.log("SUCCESS!", response.status, response.text);
+            alert('Your email is sent!');
         },
         function(error) {
             console.log("FAILED", error);
+            alert('Oops, an error occured!')
         }
     );
-    return false;  // To block from loading a new page
+    return false;  // To block from loading a new page when sent
 }
+
 
