bf5535d1bbe9feb3f812519cb2684585f8b14209
LivHed <livia.hedin@gmail.com>
2020-05-28 17:39:24 +0200

    Add functionality for ńavbar collapse and year updates for footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
-$(document).ready(function(){
-
-    $(".nav-link").click(function(){
-      $(".collapse").collapse('hide');
-    });
+$(document).ready(function () {
+    
+    // When the user clicks on a link in the navbar on smaller screens the navbar collapse.
+    $('.navbar-nav a').click(function(){
+            $(".navbar-collapse").collapse('hide');
+        });
+     
+    // Get the current year for Copyright in the footer.
+    $("#thisyear").html(new Date().getFullYear());
+     
 });
