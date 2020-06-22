b640b9fbd846fdbabc013636bf3b6d234734b002
LivHed <livia.hedin@gmail.com>
2020-06-22 18:29:23 +0200

    Added tooltips to the first 2 work project icons as an example what happends when you click them

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,12 @@ $(document).ready(function () {
      
     // Get the current year for Copyright in the footer.
     $("#thisyear").html(new Date().getFullYear());
+
+
+    // Tooltips Initialization - on the first two work card icons, as an example to show
+    // what happends when you click these icons in every work card.
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+  })
      
 });
